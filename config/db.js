3a9cda0ea17a8dfb9e0f24de973ba4427d040e5a
Abdullah-Zafar007 <161499653+Abdullah-Zafar007@users.noreply.github.com>
2025-06-26 17:14:09 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/speech_therapy", {
-    
+    {
+    await mongoose.connect(process.env.MONGO_URI);
+
     });
     console.log("MongoDB connected");
   } catch (error) {
